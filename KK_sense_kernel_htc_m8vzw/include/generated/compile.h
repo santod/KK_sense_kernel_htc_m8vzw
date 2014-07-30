985088cd3fc884e075d6cc2e0b9353416197d5f1
santod <santod040@gmail.com>
2014-07-30 11:28:14 -0700

    Initial 4.4.3 commit for m8vzw

---
+++ KK_sense_kernel_htc_m8vzw/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jul 29 08:16:11 PDT 2014"
+#define LINUX_COMPILE_BY "santod"
+#define LINUX_COMPILE_HOST "DevBoX"
+#define LINUX_COMPILER "gcc version 4.9.1 20140602 (prerelease) (GCC) "
---
+++ KK_sense_kernel_htc_m8vzw/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-NuK3rn3l_m8vzw_v2.00"
